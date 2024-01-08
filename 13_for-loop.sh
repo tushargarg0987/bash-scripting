84a1b76632e61010a55c8c5fc17409f9c774b69e
Tushar <tushargarg0987@gmail.com>
2024-01-08 12:33:15 +0530

    added: indexed for loop

--- 13_for-loop.sh
+++ 13_for-loop.sh
@@ -1,12 +1,25 @@
 #!/bin/bash
 
 #Wildcard can be used
+echo -e "\nPrinting array using wildcard: "
 for i in {1..10}
 do
 	echo "Number is: $i"
 done
 
+echo -e "\nPrinting array declared normally: "
 for name in Ram Sham Raju
 do
 	echo "Name is: $name"
 done
+
+#For loop with fixed length
+arr=( 1 2 3 Hey buddy)
+length=${#arr[*]}
+
+echo -e "\nPrinting array with index number: "
+for (( i=0; i<$length; i++))
+do
+	echo "Array value: ${arr[i]}"
+done
+
