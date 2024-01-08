1d2c53322e619d94807aa2115bfa35945ebf89dc
Tushar <tushargarg0987@gmail.com>
2024-01-08 12:40:22 +0530

    added: basics

---
+++ 01_Hello-world.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+#The line written above is called shebang line
+
+#echo can be used to print on terminal
+
+echo "Hello World!"
+
---
+++ 02_comments.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#This is a single line comment
+
+<<keyword
+This is
+a multiline
+comment
+keyword
+
+#The <<key_word is used for multiline comment where keyword can be replaced by any word
+
+echo "This file shows commenting"
